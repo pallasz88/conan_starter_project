670af0ff43c9cf6c48ab52378404cebd87413964
Paál László <pallasz88@gmail.com>
2025-12-23 18:34:20 +0000

    Apply clang format for all cpp files and fix clang-tidy and cppcheck args in pre-commit.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,12 +1,12 @@
-#include <gtest/gtest.h>
 #include "myInclude.hpp"
+#include <gtest/gtest.h>
 
 TEST(TestMain, BasicTest) {
-    print();
-    EXPECT_EQ(1 + 1, 2);
+  print();
+  EXPECT_EQ(1 + 1, 2);
 }
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
